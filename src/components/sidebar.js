8647c6b25ab0584c9caaf8a1df3b0b79e36b2128
rBurgett <ryan@burgettweb.net>
2018-08-14 12:09:13 -0400

    Sorted channels in sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -46,49 +46,81 @@ const Sidebar = ({ selectedChannel, channels, videos, onAddChannelClick, onChann
     }
   };
 
-  const items = channels.map(c => {
+  const videosObj = videos
+    .sort((a, b) => -1 * a.isoDate.localeCompare(b.isoDate))
+    .reduce((obj, v) => {
+      if(obj[v.channel]) {
+        return {
+          ...obj,
+          [v.channel]: [
+            ...obj[v.channel],
+            v
+          ]
+        };
+      } else {
+        return {
+          ...obj,
+          [v.channel]: [v]
+        };
 
-    const unplayedLength = videos
-      .filter(v => v.channel === c._id)
-      .filter(v => !v.played)
-      .length;
+      }
+    }, {});
 
-    const onClick = e => {
-      e.preventDefault();
-      onChannelClick(c._id);
-    };
+  const items = channels
+    .sort((a, b) => {
+      const aVideos = videosObj[a._id] || [];
+      const bVideos = videosObj[b._id] || [];
+      if(aVideos.length === 0) {
+        return 1;
+      } else if(bVideos.length === 0) {
+        return -1;
+      } else {
+        return -1 * aVideos[0].isoDate.localeCompare(bVideos[0].isoDate);
+      }
+    })
+    .map(c => {
 
-    const onContextMenu = e => {
-      e.preventDefault();
-      const menu = Menu.buildFromTemplate([
-        {
-          label: 'Mark all watched',
-          click: async function() {
-            try {
-              onMarkAllWatched(c._id);
-            } catch(err) {
-              handleError(err);
+      const unplayedLength = videos
+        .filter(v => v.channel === c._id)
+        .filter(v => !v.played)
+        .length;
+
+      const onClick = e => {
+        e.preventDefault();
+        onChannelClick(c._id);
+      };
+
+      const onContextMenu = e => {
+        e.preventDefault();
+        const menu = Menu.buildFromTemplate([
+          {
+            label: 'Mark all watched',
+            click: async function() {
+              try {
+                onMarkAllWatched(c._id);
+              } catch(err) {
+                handleError(err);
+              }
+            }
+          },
+          {
+            label: 'Delete Channel',
+            click: async function() {
+              try {
+                const { value: confirmed } = await swal({
+                  type: 'warning',
+                  text: `Are you sure that you want to delete ${c.title}? If you continue, this channel and all associated videos will be removed.`,
+                  showCancelButton: true
+                });
+                if(confirmed) onDeleteChannel(c._id);
+              } catch(err) {
+                handleError(err);
+              }
             }
           }
-        },
-        {
-          label: 'Delete Channel',
-          click: async function() {
-            try {
-              const { value: confirmed } = await swal({
-                type: 'warning',
-                text: `Are you sure that you want to delete ${c.title}? If you continue, this channel and all associated videos will be removed.`,
-                showCancelButton: true
-              });
-              if(confirmed) onDeleteChannel(c._id);
-            } catch(err) {
-              handleError(err);
-            }
-          }
-        }
-      ]);
-      menu.popup({});
-    };
+        ]);
+        menu.popup({});
+      };
 
     return (
       <li key={c._id} className={'nav-item'}>
